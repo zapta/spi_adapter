74154d606661ef8e419749669c2f71579ad3c3f2
Zapta <zapta@users.noreply.github.com>
2024-01-15 20:46:33 -0800

    Added an ADC example.

---
+++ examples/adc_demo.py
@@ -0,0 +1,27 @@
+"""ADC ADS1118 demo."""
+
+import sys
+import time
+
+sys.path.insert(0, '../src/')
+from spi_adapter import SpiAdapter
+
+port = "COM18"
+
+print(f"Connecting to port {port}...", flush=True)
+spi =  SpiAdapter(port = port)
+print(f"Connected.", flush=True)
+
+# Single shot, 2.046v FS, A0 input.
+config_byte_msb = 0b11000101
+config_byte_lsb = 0b10001010
+cmd = bytes([config_byte_msb, config_byte_lsb, 0x00, 0x00])
+
+while True:
+  resp = spi.send(cmd)
+  assert isinstance(resp, bytes)
+  assert len(resp) == 4
+  print(f"{i:04d} Response: {result.hex(' ')}", flush=True)
+  value = int.from_bytes(resp[0:2], byteorder='big', signed=True)
+  time.sleep(0.5)
+
