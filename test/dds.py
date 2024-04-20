6e5ae3dc4c6535b33998ae5740f44b590d692ea5
Zapta <zapta@users.noreply.github.com>
2024-04-19 22:09:44 -0700

    DDS on/off pattern works.

--- test/dds.py
+++ test/dds.py
@@ -3,91 +3,106 @@
 # See example here
 # https://github.com/RobTillaart/AD9833/blob/master/AD9833.cpp
 
-import sys
 import time
 from spi_adapter import SpiAdapter
+from typing import List
 
-#port = "COM18"
-port = "/dev/tty.usbmodem101"
-
-spi: SpiAdapter = None
-
-dds_on: bool = False
-
-def ones(num_bits: int) -> int:
-    """Construct a bit mask with N LSB bits set."""
-    assert isinstance(num_bits, int)
-    assert 0 <= num_bits <= 16
-    result = int((1 << (num_bits)) - 1)
-    assert isinstance(result, int)
-    assert 0 <= result <= 0xffff,  f"{num_bits = }, {result = }"
-    return result
+# port = "COM18"
+port = "/dev/tty.usbmodem1101"
 
 
-def bits(*bit_indexes: int) -> int:
-    """Construct a bit mask with given bits set."""
-    result: int = 0
-    for bit_index in bit_indexes:
-        assert isinstance(bit_index, int), type(bit_index)
-        assert 0 <= bit_index <= 15
-        result = result | (1 << bit_index)
-    assert isinstance(result, int)
-    assert 0 <= result <= ones(16)
-    return result
 
+class Dds:
+    def __init__(self, port: str):
+        self.__port = port
+        self.__spi = None
 
-def send_words(*words: int) -> None:
-    """Send to the DDS device N 16 bit words over SPI."""
-    global spi
-    assert len(words) > 0
-    data_bytes = bytearray()
-    for w in words:
-        assert isinstance(w, int), type(w)
-        assert 0 <= w <= ones(16)
-        data_bytes.append(w >> 8)
-        data_bytes.append(w & ones(8))
-    result = spi.send(data_bytes, mode=2,  read=False)
-    assert result is not None
+    @classmethod
+    def ones(cls, num_bits: int) -> int:
+        """Construct a bit mask with N LSB bits set."""
+        assert isinstance(num_bits, int)
+        assert 0 <= num_bits <= 16
+        result = int((1 << (num_bits)) - 1)
+        assert isinstance(result, int)
+        assert 0 <= result <= 0xffff, f"{num_bits = }, {result = }"
+        return result
 
+    @classmethod
+    def bits(cls, *bit_indexes: int) -> int:
+        """Construct a bit mask with given bits set."""
+        result: int = 0
+        for bit_index in bit_indexes:
+            assert isinstance(bit_index, int), type(bit_index)
+            assert 0 <= bit_index <= 15
+            result = result | (1 << bit_index)
+        assert isinstance(result, int)
+        assert 0 <= result <= cls.ones(16)
+        return result
 
-def dds_reset() -> None:
-    global spi, dds_on
-    dds_on = False
-    send_words(bits(8))
-    send_words(bits())
+    def __send_cmd_words(self, *words: int) -> None:
+        """Send to the DDS device N 16 bit words over SPI."""
+        assert self.__spi is not None
+        assert len(words) > 0
+        data_bytes = bytearray()
+        for w in words:
+            assert isinstance(w, int), type(w)
+            assert 0 <= w <= self.ones(16)
+            data_bytes.append(w >> 8)
+            data_bytes.append(w & self.ones(8))
+        result = self.__spi.send(data_bytes, mode=2, read=False)
+        assert result is not None
 
+    def __frequency_cmd_words(self, freq_hz: int) -> List[int]:
+        """Return the command words to set the DDS frequency"""
+        # Compute DDS freq register value
+        assert isinstance(freq_hz, int)
+        assert 0 <= freq_hz <= 20000
+        freq_reg_val = int((1 << 28) * freq_hz / 25_000_000)
+        assert 0 <= freq_reg_val < (1 << 28)
+        # Write to the config register and the frequency reg.
+        words = []
+        # Config reg.
+        words.append(self.bits(13))
+        # Freq0 LSB, MSB words.
+        words.append(self.bits(14) | freq_reg_val & self.ones(14))
+        words.append(self.bits(14) | freq_reg_val >> 14)
+        return words
 
-def dds_set_frequency(f: int) -> None:
-    global spi
-    # Compute DDS freq register value
-    assert isinstance(f, int)
-    assert 20 <= f <= 20000
-    freq_reg_val = int((1 << 28) * 1000 / 25_000_000)
-    assert 0 <= freq_reg_val < (1 << 28)
-    # Write to the config register and the frequency reg.
-    words = []
-    words.append(bits(13))
-    words.append(bits(14) | freq_reg_val & ones(14))
-    words.append(bits(14) | freq_reg_val >> 14)
-    send_words(*words)
+    def connect(self) -> None:
+        print(f"Connecting DDS SPI to port {self.__port}.", flush=True)
+        assert self.__spi is None
+        self.__spi = SpiAdapter(port=self.__port)
+        assert self.__spi is not None
+
+    def reset(self) -> None:
+        """Reset the DDS. Output is disabled upon return and
+        ADC is at mid point."""
+        assert self.__spi is not None
+        # Set frequency to zero.
+        words = self.__frequency_cmd_words(0)
+        # Tobble config reset bit on and then off.
+        words.append(self.bits(8))
+        words.append(self.bits())
+        self.__send_cmd_words(*words)
+
+    def set_frequency(self, freq_hz: int) -> None:
+        assert self.__spi is not None
+        words = self.__frequency_cmd_words(freq_hz)
+        self.__send_cmd_words(*words)
 
 
 def main() -> None:
-    global spi
-    print(f"Connecting to port {port}...", flush=True)
-    spi = SpiAdapter(port=port)
-    print(f"Connected.", flush=True)
+    dds = Dds(port)
+    dds.connect()
 
     print(f"Resetting DDS device", flush=True)
-    dds_reset()
+    dds.reset()
 
-    print(f"Setting DDS frequency", flush=True)
-    dds_set_frequency(2000)
-    print(f"Done")
-
-    #while True:
-    #    time.sleep(5.0)
-    #    print("loop", flush=True)
+    while True:
+        dds.set_frequency(2000)
+        time.sleep(0.1)
+        dds.reset()
+        time.sleep(0.1)
 
 
 if __name__ == "__main__":
